592fffa1ccecc260dbd70a64eaf986b1097800f1
sheetal-10 <sheetalp257@gmail.com>
2021-08-17 20:12:42 +0530

    Added : code for finding maximum of three integers

--- src/com/generics/Maximum.java
+++ src/com/generics/Maximum.java
@@ -1,8 +1,41 @@
 package com.generics;
 
-
+/**
+ * Program to find the maximum of given three integers
+ * @author Sheetal
+ * @since 2021-08-17
+ */
 public class Maximum {
+	/**
+	 * Main method where 
+	 * variables are declare and initialise and
+	 * calling findMaximum method to main
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
-		System.out.println("welcome to generics maximum finder");
-		}
+		//declaring and initialising varaibles
+		Integer a=2; 
+		Integer b=4; 
+		Integer c=6;
+		//calling method to main
+		findMaximum(a,b,c);
 	}
+		
+	/**
+	 * Method to find Maximum of given three integers using compare to
+	 * @param a 
+	 * @param b
+	 * @param c
+	 * @return maximum of a,b,c
+	 */
+	private static void findMaximum(Integer a, Integer b, Integer c) {
+			Integer max = a;
+			if (b.compareTo(a)>0) {
+				max = b;
+			}
+			if (c.compareTo(max)>0) {
+				max = c;
+			}
+		 System.out.println("Maximum of numbers are " +max);
+	}
+}
