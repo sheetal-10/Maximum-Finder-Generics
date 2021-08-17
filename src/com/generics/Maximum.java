49f29687121fe61944bb1340acc4f8a619c9e680
sheetal-10 <sheetalp257@gmail.com>
2021-08-17 20:20:06 +0530

    Added: code for finding maximum of three floats

--- src/com/generics/Maximum.java
+++ src/com/generics/Maximum.java
@@ -1,7 +1,7 @@
 package com.generics;
 
 /**
- * Program to find the maximum of given three integers
+ * Program to find the maximum of given three floats
  * @author Sheetal
  * @since 2021-08-17
  */
@@ -14,28 +14,28 @@ public class Maximum {
 	 */
 	public static void main(String[] args) {
 		//declaring and initialising varaibles
-		Integer a=2; 
-		Integer b=4; 
-		Integer c=6;
+		Float x = 3.4f;
+		Float y = 4.6f;
+		Float z = 6.7f;
 		//calling method to main
-		findMaximum(a,b,c);
+		findMaximum(x,y,z);
 	}
 		
 	/**
-	 * Method to find Maximum of given three integers using compare to
-	 * @param a 
-	 * @param b
-	 * @param c
-	 * @return maximum of a,b,c
+	 * Method to find Maximum of given three floats using compare to
+	 * @param x 
+	 * @param y
+	 * @param z
+	 * @return maximum of x,y,z
 	 */
-	private static void findMaximum(Integer a, Integer b, Integer c) {
-			Integer max = a;
-			if (b.compareTo(a)>0) {
-				max = b;
-			}
-			if (c.compareTo(max)>0) {
-				max = c;
-			}
+	private static void findMaximum(Float x, Float y, Float z) {
+		Float max = x;
+		if (y.compareTo(x)>0) {
+			max = y;
+	}
+		if (z.compareTo(max)>0) {
+			max = z;
+		}
 		 System.out.println("Maximum of numbers are " +max);
 	}
 }
