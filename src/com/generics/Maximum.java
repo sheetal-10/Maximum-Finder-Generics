2dee1934adcd6ae68b0a7de6eea6f95faf57c91c
sheetal-10 <sheetalp257@gmail.com>
2021-08-23 10:21:15 +0530

    Added: code for method level generics

--- src/com/generics/Maximum.java
+++ src/com/generics/Maximum.java
@@ -3,40 +3,25 @@ package com.generics;
 /**
  * Program to find the maximum of given three strings
  * @author Sheetal
- * @since 2021-08-17
+ * @since 2021-08-18
  */
 public class Maximum {
 	/**
-	 * Main method where 
-	 * variables are declare and initialise and
-	 * calling findMaximum method to main
+	 * Method where 
+	 * we are using method level generics to
+	 * find maximum of three numbers
 	 * @param args unused
 	 */
-	public static void main(String[] args) {
-		//declaring and initialising varaibles
-		String l = "apple";
-		String m = "banana";
-		String n = "orange";
-		//calling method to main
-		findMaximum(l,m,n);
+	 public static <T extends Comparable> T findMax(T num1, T num2, T num3) {
+	        T max = num1;
+	        if (num2.compareTo(max) > 0) {
+	            max = num2;
+	        }
+	        if (num3.compareTo(max) > 0) {
+	            max = num3;
+	        }
+	        System.out.printf("Maximum of numbers are " +max);
+	        return max;
+	    }
 		
-		
-	}
-		
-	/**
-	 * Method to find Maximum of given three strings using compare to
-	 * @param l 
-	 * @param m
-	 * @param n
-	 * @return maximum of l,m,n
-	 */
-	private static void findMaximum(String l, String m, String n) {
-		String max = l;
-		if (m.compareTo(l)>0) {
-			max = m;
-		}
-		if (n.compareTo(max)>0) {
-			max = n;
-		}		 System.out.println("Maximum of strings-" +max);
-	}
 }
