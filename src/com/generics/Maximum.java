8f7af8be704d01eb89b1a6a87db4e4444d872cef
sheetal-10 <sheetalp257@gmail.com>
2021-08-23 10:43:54 +0530

    Added: code to find maximum using array

--- src/com/generics/Maximum.java
+++ src/com/generics/Maximum.java
@@ -2,49 +2,49 @@ package com.generics;
 
 /**
  * Program to find the maximum of given three variables 
- * using class level generics
+ * using class level generics using array
  * @author Sheetal
  * @since 2021-08-18
  */
 public class Maximum <T extends Comparable<T>>{
-	//variables
-	T num1;
-	T num2;
-	T num3;
+	//generics array
+	private T[] inputArray;
+	
+	//constructor
+	public Maximum(T[] inputArray) {
+	        this.inputArray=inputArray;
+	    }
+		
+	//method to find max of array of any data type
+	private static <T extends Comparable> void getMaximum(T[] array){
+	        T max = array[0];
+	        for (int i=0;i<array.length; i++){
+	            if (array[i].compareTo(max)>0){
+	                max=array[i];
+	            }
+	        }
+	        System.out.println("Maximum : "+max);
+	    }
 	
 	/**
-	 * main method where 
-	 * variables are assigned values
-	 * and printing the maximum of the given values
+	 * main method where
+	 * we are declaring and initialising array of integer , double and string and
+	 * printing the maximum of respective data
 	 * @param args unused
 	 */
 	public static void main(String[] args) {
-   	  System.out.println("Maximum Integer by creating Generic class :" + new Maximum(4, 6, 8).GenericMax());
-   	  System.out.println("Maximum Float by creating Generic class :" + new Maximum(2.5f, 4.56f, 3.45f).GenericMax());
-   	  System.out.println("Maximum Float by creating Generic class :" + new Maximum("lotus","rose","lily").GenericMax());
-   }
-	
-	 //constructor
-    public Maximum(T num1, T num2, T num3) {
-		super();
-		this.num1 = num1;
-		this.num2 = num2;
-		this.num3 = num3;
-	}
-
-
-    public T GenericMax() {
-    	return Maximum.GenericMax(num1, num2, num3);
-    }
-
-    //generics method to compare three varaibles and find maximum
-    public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
-        T max = num1;
-        if (num2.compareTo(max) > 0)
-            max = num2;
-        if (num3.compareTo(max) > 0)
-            max = num3;
-        return max;
-    }
-		
+	        // Declaring array of  INTEGER,FLOAT,STRING
+	    	Integer[] intArray={10,9,8,7,6,5,56};
+	        Double[] doubleArray={3.3,6.67,4.56,89.43};
+	        String[] stringArray={"a","abc","abcgss","abcde"};
+	        
+	        System.out.println("maximum of integer [10,9,8,7,6,5,56] values are");
+	        Maximum.getMaximum(intArray);
+	        
+	        System.out.println("maximum of double [3.3,6.67,4.56,89.43] values are");
+	        Maximum.getMaximum(doubleArray);
+	        
+	        System.out.println("maximum of string [ a,abc,abcgss,abcde ] values are");
+	        Maximum.getMaximum(stringArray);
+	    }
 }
